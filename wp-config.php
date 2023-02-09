9afafd5518ec947ef9b374ee6f65c73ca1fa937d
Olatoye <olatoye@Olatoyes-MacBook-Pro.local>
2023-02-08 21:13:17 -0800

    rds changed

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpressuser' );
 define( 'DB_PASSWORD', 'W3lcome123' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'wordpressdbclixxjenkins.cudbu62q5h0d.us-east-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'wordpressdbclixxjenkins2.cudbu62q5h0d.us-east-1.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
